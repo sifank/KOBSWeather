b60bbb43b46daff0c1e78d393d419f83f3814451
sifank <sifan.kahale@gmail.com>
2021-03-11 14:41:18 -0800

    clarified instructions and cleaned up a tad

--- KOBSweather.cpp
+++ KOBSweather.cpp
@@ -39,12 +39,6 @@
 // We declare an auto pointer to KOBSweather.
 std::unique_ptr<KOBSweather> openWeatherMap(new KOBSweather());
 
-//static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-//{
-    //((std::string *)userp)->append((char *)contents, size * nmemb);
-    //return size * nmemb;
-//}
-
 void ISGetProperties(const char *dev)
 {
     openWeatherMap->ISGetProperties(dev);
