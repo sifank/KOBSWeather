791c12a9b24811fa1c70f024eccb45403411e98a
Sifan <40527717+sifank@users.noreply.github.com>
2025-11-28 16:01:25 -0800

    Add files via upload
    KOBSweather.py now has 2 versions: kobs and field

---
+++ KOBSweather-field.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+# File:         KOBSweather-field.py
+# Version:      20251128
+# Authur:       Sifan
+# Desc:         retrieves weather data from mysql for the INDI KOBSweather driver
+
+import sys, os
+import mysql.connector
+
+MYHost = "kiana"
+MYUser = "sifan"
+MYPwd = "all4Sky"
+MYDb = "kahaleobs"
+MYTbl = "sensordata"
+
+#--------------------------------------------------------
+# Main - initialize
+#--------------------------------------------------------
+# open up mysql connector
+try:
+    conn = mysql.connector.connect (host=MYHost, user=MYUser, passwd=MYPwd, db=MYDb)
+    cursor = conn.cursor()
+except mysql.connector.Error as e:
+    print("DB Error opening db %d:  %s" % (e.args[0], e.args[1]))
+    sys.exit(1)
+
+#------------------------------------------------------
+# Create data file and print html page
+#------------------------------------------------------
+# read in from db
+try:
+    cursor.execute ("SELECT * FROM %s ORDER BY time DESC LIMIT 1" % MYTbl)
+except:
+    print("DB Error reading servers table")
+    sys.exit(1)
+
+# read db and create csv records
+for (time,
+  otaTemp,
+  otaHum,
+  otaDp,
+  otaPres,
+  otaLux,
+  otaSkyTemp,
+  allskyTemp,
+  allskyHum,
+  allskyDp,
+  allskyPres,
+  allskyLux,
+  allskySkyTemp,
+  allskyWind,
+  allskyWGust,
+  allskyWDir,
+  fallskyTemp,
+  fallskyHum,
+  fallskyDp,
+  fallskyPres,
+  fallskyLux,
+  fallskySkyTemp,
+  fallskyWind,
+  fallskyWGust,
+  fallskyWDir,
+  obsTemp,
+  obsHum,
+  obsDp,
+  obsPres,
+  obsWind,
+  obsGust,
+  obsWDir,
+  obsIsRain,
+  obsRain,
+  obsRain1Hr,
+  obsRain24Hr,
+  obsLgtng,
+  domeTemp,
+  domeHum,
+  domeDp,
+  domePres,
+  domeIsRain,
+  domePME10,
+  domePME25,
+  domePME100,
+  domePM3,
+  domePM5,
+  domePM10,
+  domeRainMin,
+  domeRainHr,
+  domeRainDy,
+  domeRainMonth,
+  domeRainYear) in cursor:
+
+  # rain comes in as y or n, but needs to be int
+  if (obsIsRain == "y"):
+        Raining = 1
+  else:
+        Raining = 0
+        
+  print("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (fallskyTemp, fallskyHum, fallskyDp, fallskyPres, fallskyWind, fallskyWGust, obsRain, Raining, fallskySkyTemp, fallskyLux))
+    
+cursor.close()
+
